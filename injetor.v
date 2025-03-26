6ef02fc5d09e351587a8c3026a01be4da3ca6771
Rodolfo Azevedo <rodolfo.azevedo@unicamp.br>
2025-03-26 15:52:34 -0300

    versão completa da atividade

---
+++ injetor.v
@@ -0,0 +1,10 @@
+module injetor(
+  input [8:0] entrada,
+  input [3:0] n,
+  input erro,
+  output reg [8:0] saida
+);
+
+// insira seu código aqui
+
+endmodule
---
+++ template/verifica_paridade.v
@@ -0,0 +1,8 @@
+module verifica_paridade (
+  input [8:0] dado, // a paridade é o bit mais significativo (dado[8])
+  output erro
+);
+
+// implemente o seu código aqui
+
+endmodule
